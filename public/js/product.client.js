fb39b837f13cd8d13bb00926221334f6fd81f6d1
Nilesh410 <shirudenileshs@gmail.com>
2023-12-15 12:26:02 +0530

    add details inside the table

--- public/js/product.client.js
+++ public/js/product.client.js
@@ -1,12 +1,14 @@
 window.addEventListener("load",()=>{
-    
+    let list=[]
+    let tbody=document.querySelector("#tbody")
     async function getProductDetails(){
         let url=`http://localhost:3051/get-product`;
         let response= await fetch(url, {method: "GET" });
         let data=await response.json()
         if(data.status===true)
         {
-            console.log(data.result)
+            list=data.result
+            printPrdDetails(list)
         }
         else
         {
@@ -14,7 +16,19 @@ window.addEventListener("load",()=>{
         }
 
     }
+    function printPrdDetails(list)
+    {
+        tbody.innerHTML=list.map((product,index)=>{
+            return ` <tr>
+                      <th scope="row">${index+1}</th>
+                      <td>${product.productname}</td>
+                      <td>${product.qty}</td>
+                      <td>${product.price}</td>
+                    </tr>`
+
+        }).join("")
+    }
     getProductDetails()
 
-    <tr><th scope="row">1</th><td>Mark</td><td>Otto</td><td>@mdo</td></tr>
+   
 })
